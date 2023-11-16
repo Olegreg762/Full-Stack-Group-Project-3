5aa06127efa44e4e6b74934db9a64adb6b9aa407
Olegreg762 <109924953+Olegreg762@users.noreply.github.com>
2023-11-15 19:12:50 -0600

    create User,Book models. begin library model

--- library-app/server/config/connection.js
+++ library-app/server/config/connection.js
@@ -1,6 +1,4 @@
 const mongoose = require('mongoose');
 
-module.exports = mongoose.connect(process.env.MONGODB_URI ||
-                    // Change to name of project
-     'mongodb://127.0.0.1:27017/PLACEHOLDER');
+module.exports = mongoose.connect(process.env.MONGODB_URI ||'mongodb://127.0.0.1:27017/CHLOE');
 

3a24e14664471d6f4107bd11ec9c8dfe8f9073af
Brett C <brcannon@bellsouth.net>
2023-11-15 19:09:15 -0600

    updates to client side folders and started with pathing to pages

--- library-app/client/src/main.jsx
+++ library-app/client/src/main.jsx
@@ -2,9 +2,34 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { createBrowswerRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowswerRouter([
+  {
+    path:"/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+      element: <LoginPage />
+    }, {
+      path: '/profiles/:profileId',
+      element: <Profile />
+    }, {
+      path: '/libraries',
+      element: <LibraryHub />
+    }, {
+      path: '/library/:libraryId',
+      element: <LibraryPage />
+    }
+    ]
+  }
+])
+
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <RouterProvider router={router} />
 )

a4ef1d975ce349c9e0843f4bc296f4ce09d897be
kevinatwood <atwoodkevin22@gmail.com>
2023-11-16 18:26:16 -0600

    database and seeds are working!

--- library-app/server/seeds/seed.js
+++ library-app/server/seeds/seed.js
@@ -1,11 +1,17 @@
 const db = require('../config/connection');
 const { Book, Library, User} = require('../models');
+// const Book = require('../models/Book')
 const cleanDB = require('./cleanDB');
 const bookData = require('./bookData.json')
+const libraryData= require('./libraryData.json')
+const userData = require('./userData.json')
 
 db.once('open', async () => {
      await cleanDB("Book", 'books');
-
+     await cleanDB("User", 'users');
+     await cleanDB("Library",'libraries')
+    await User.insertMany(userData)
+    await Library.insertMany(libraryData)
     await Book.insertMany(bookData)
     console.log("seeded")
     process.exit(0)

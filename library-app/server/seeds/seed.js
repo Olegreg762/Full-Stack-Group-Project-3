fbf736dc9fe9dd9fab94adb7d9a8a39f81c8c162
Olegreg762 <109924953+Olegreg762@users.noreply.github.com>
2023-11-16 19:17:08 -0600

    fix library seed bug

--- library-app/server/seeds/seed.js
+++ library-app/server/seeds/seed.js
@@ -7,12 +7,31 @@ const bookData = require('./bookData.json')
 const libraryData= require('./libraryData.json')
 const userData = require('./userData.json')
 
+async function createLibraryStart() {
+    try {
+        const teacherUsers = await User.find({ isteacher: true});
+        
+        const teachers = teacherUsers.map(async (teacher) => { 
+            const libraryname = `${teacher.username}'s Library`;
+            const library = new Library({
+                libraryname: libraryname,
+                libraryowner: teacher._id
+            });
+
+        await library.save();
+        });
+    } catch (error) {
+        console.log(error)
+    }
+}
+
 db.once('open', async () => {
-     await cleanDB("Book", 'books');
-     await cleanDB("User", 'users');
-     await cleanDB("Library",'libraries')
+    await cleanDB("Book", 'books');
+    await cleanDB("User", 'users');
+    await cleanDB("Library",'libraries')
     await User.insertMany(userData)
-    await Library.insertMany(libraryData)
+    // await Library.insertMany(libraryData)
+    createLibraryStart();
     await Book.insertMany(bookData)
     console.log("seeded")
     process.exit(0)

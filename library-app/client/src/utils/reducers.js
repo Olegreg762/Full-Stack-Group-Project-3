ed985b71dd65528ff7dcc088c35fd8e60c4a5473
Tim <135084118+capydays@users.noreply.github.com>
2023-11-16 19:21:25 -0600

    additional reducers

--- library-app/client/src/utils/reducers.js
+++ library-app/client/src/utils/reducers.js
@@ -11,10 +11,35 @@ import {
 
 export const reducer = (state, action) => {
     switch (action.type) {
+        // Need assitance on figuring out checkout logic and process
         case CHECKOUT_BOOK:
+            let newLibrary = state.library.filter((book) => {
+                return book._id !== action._id
+            });
             return {
                 ...state,
-                books: [...action.books]
+                books: newLibrary
             }
+
+        case RETURN_BOOK:
+            return {
+                ...state,
+                books: [...state.library, action.book]
+            }
+        
+        case VIEW_BOOKS:
+            return {
+                ...state,  
+            }
+        
+        case REVIEW_BOOK:
+
+        case ADD_BOOK:
+
+        case DELETE_BOOK:
+
+        case ADD_LIBRARY:
+
+        case DELETE_LIBRARY:
     }
 }

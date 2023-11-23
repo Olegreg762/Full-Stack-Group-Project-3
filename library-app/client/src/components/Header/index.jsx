8d6748858d130a8431d4a1a1370cabda2524c537
kevinatwood <atwoodkevin22@gmail.com>
2023-11-22 22:04:24 -0600

    fixed header to not show links if user is not logged in

--- library-app/client/src/components/Header/index.jsx
+++ library-app/client/src/components/Header/index.jsx
@@ -1,20 +1,10 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
+import auth from "../../utils/auth";
 
 const Header = () => {
 
   return (
-    // <header className="header">
-    //   <div className="">
-    //   <Link to="/profile"><button className="btn" >
-    //       Profile
-    //    </button></Link>
-    //    <Link to= "/libraries"><button className="btn" >
-    //       Libraries
-    //    </button>
-    //    </Link>
-    //   </div>
-    // </header>
 
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
 <div class="container-fluid">
@@ -22,6 +12,8 @@ const Header = () => {
   <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
+  
+  {auth.loggedIn() ?
   <div class="collapse navbar-collapse" id="navbarNav">
     <ul class="navbar-nav">
       <li class="nav-item">
@@ -34,7 +26,7 @@ const Header = () => {
        <Link to='/libraries'> <a class="nav-link">Libraries</a></Link>
       </li>
     </ul>
-  </div>
+  </div> : null}
 </div>
 </nav>
   );

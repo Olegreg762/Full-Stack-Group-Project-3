f44e28a6e87196481707ec15b31bcb01cfa75a62
kevinatwood <atwoodkevin22@gmail.com>
2023-11-22 19:13:37 -0600

    finished mutations?

--- library-app/client/src/utils/actions.js
+++ library-app/client/src/utils/actions.js
@@ -6,4 +6,16 @@ export const ADD_BOOK = "ADD_BOOK";
 export const DELETE_BOOK = "DELETE_BOOK";
 export const ADD_LIBRARY = "ADD_LIBRARY";
 export const DELETE_LIBRARY = "DELETE_LIBRARY";
+export const LOGIN = "LOGIN";
+export const ADD_USER = "ADD_USER"
+export const BOOK_CHECKOUT = "BOOK_CHECKOUT"
+export const BOOK_RETURN = "BOOK_RETURN"
+export const ADD_BOOK_TO_LIBRARY = "ADD_BOOK_TO_LIBRARY"
+export const REMOVE_BOOK_FROM_LIBRARY = "REMOVE_BOOK_FROM_LIBRARY"
+export const UPDATE_BOOK = "UPDATE_BOOK"
+export const DELETE_USER = "DELETE_USER"
+export const UPDATE_USER = "UPDATE_USER"
+export const UPDATE_LIBRARY = "UPDATE_LIBRARY"
+
+
 

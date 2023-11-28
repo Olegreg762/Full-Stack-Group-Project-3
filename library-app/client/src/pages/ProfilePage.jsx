bd0fd98b7855770e9cc08f44eee05d243b671a8b
kevinatwood <atwoodkevin22@gmail.com>
2023-11-27 18:31:37 -0600

    made some conditional rendering of profile page

--- library-app/client/src/pages/ProfilePage.jsx
+++ library-app/client/src/pages/ProfilePage.jsx
@@ -1,63 +1,35 @@
 import React from "react"
 import logo from '../assets/image/profilePH.jpg'
 import ProgressBar from "../components/ProgressBar"
+import auth from "../utils/auth"
+import { QUERY_ONE_USER } from "../utils/queries"
+import { useQuery } from "@apollo/client"
+import TeacherProfile from "../components/Profile/teacherProfile"
+import StudentProfile from "../components/Profile/studentProfile"
 
 const testData = [
     { bgcolor: "#6a1b9a", completed: 60 }
 ]
-
+// TODO: create teacher profile page with list of libraries that takes you to the library page
+//TODO: refactor student profile page with user info,list of checked out books, books already read, and a progress bar showing how many books have been read with a goal of 30
 const ProfilePage = () => {
+    const {data, loading} = useQuery(QUERY_ONE_USER, {
+        variables: {
+            id: auth.getProfile().data._id
+        }
+    })
+
+    const user = data?.user || {}
+    if (loading){
+        return (<h2>Loading...</h2>)
+    } else {
+        console.log(data)
+    }
     return (
-        <div className="">
-            <h1 className="text-center">Profile Page!</h1>
-            <div className="d-flex justify-content-around">
-                <div className="profile-info">
-                    {/* Shows profile picture and has info about profile such as name grade and favorite subject*/}
-                    <img src={logo} className="profile-logo" alt="logo" />
+     
+        <>
+        {user.isteacher ? <TeacherProfile/> : <StudentProfile/> }</>
 
-                </div>
-                <div className="profile-books border border-4 border-secondary p-5">
-                    {/*Pulls completed book from the database and uses the google api to get info, plus pulls comments */}
-
-                </div>
-                <div className="profile-goals">
-                    {/* Goal svg to update percent}
-                {Goals in number that auto increases when book is read */}
-
-                    <div className="profile-goal-svg border border-4 border-secondary p-5 d-flex justify-content-center p-5">
-                        {testData.map((item, idx) => (
-                            <ProgressBar key={idx} bgcolor={item.bgcolor} completed={item.completed} />
-                        ))}
-                    </div>
-                    <div className="profile-goal-defined">
-                        {/* div to house goal in numbers */}
-                        <div className="d-flex justify-content-center">
-                            <button
-                                type="button"
-                                className='dropdown-toggle dropdown-toggle-split m-2'
-                                data-bs-toggle="dropdown"
-                                aria-expanded="false"
-                            >
-                                GOAL!
-                            </button>
-
-                            <div className='dropdown-menu'>
-                                <a className='dropdown-item'> 1 </a>
-                                <a className='dropdown-item'> 2 </a>
-                                <a className='dropdown-item'> 3 </a>
-                                <a className='dropdown-item'> 4 </a>
-                                <a className='dropdown-item'> 5 </a>
-                                <a className='dropdown-item'> 6 </a>
-                                <a className='dropdown-item'> 7 </a>
-                                <a className='dropdown-item'> 8 </a>
-                                <a className='dropdown-item'> 9 </a>
-                                <a className='dropdown-item'> 10 </a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
     )
 }
 

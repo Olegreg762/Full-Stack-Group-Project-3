8f1be261e9de6850c0418c2fb4b8b42a713099ce
Tim <135084118+capydays@users.noreply.github.com>
2023-11-27 21:20:29 -0600

    libraryhub context

--- library-app/client/src/components/BookList/index.jsx
+++ library-app/client/src/components/BookList/index.jsx
@@ -1,11 +1,12 @@
 import LibraryHub from "../../pages/LibraryHub";
+import BookAddPage from "../../pages/BookAddPage";
 import { useLibraryContext } from "../../utils/GlobalState";
 import { ADD_BOOK_TO_LIBRARY, REMOVE_BOOK_FROM_LIBRARY } from "../../utils/actions";
 
 const AddBook = () => {
     const [state, dispatch] = useLibraryContext;
 
-    const updateOnSubmit = (e) => {
+    const updateOnClick = (e) => {
         e.preventDefault();
         dispatch({
             type: ADD_BOOK_TO_LIBRARY,
@@ -20,14 +21,46 @@ const AddBook = () => {
 
     return (
         <LibraryHub>
-            <form
-                type="form"
-                id="form"
-                className="form"
-                onSubmit={updateOnSubmit}
+            <button
+                type="button"
+                id="button"
+                className="btn"
+                onClick={updateOnClick}
             >
-                {state.payload}
-            </form>
+                Add Book
+            </button>
         </LibraryHub>
     )
-}
+};
+
+const RemoveBook = () => {
+    const [state, dispatch] = useLibraryContext;
+
+    const updateOnClick = (e) => {
+        e.preventDefault();
+        dispatch({
+            type: REMOVE_BOOK_FROM_LIBRARY,
+        });
+
+        const removeBookResults = state.isValid
+            ? 'Book successfully removed from Library!'
+            : 'Failed to remove book from Library'
+        
+            alert(removeBookResults);
+    };
+
+    return (
+        <LibraryHub>
+            <button
+                type="button"
+                id="button"
+                className="btn"
+                onClick={updateOnClick}
+            >
+                Remove Book
+            </button>
+        </LibraryHub>
+    )
+};
+
+export default { AddBook, RemoveBook };

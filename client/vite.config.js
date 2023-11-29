6e27e28e6771aeb19857496ae397f6657de7c6a9
Olegreg762 <109924953+Olegreg762@users.noreply.github.com>
2023-11-29 17:28:43 -0600

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,7 +4,7 @@ import ViteReactPlugin from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), ViteReactPlugin()],
+  plugins: [ViteReactPlugin()],
   server: {
     port: 3000,
     open: true,

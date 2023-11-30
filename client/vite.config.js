a584294948da02fed0398f42e32224fc8f13f6f7
Olegreg762 <109924953+Olegreg762@users.noreply.github.com>
2023-11-29 18:42:57 -0600

    update to vite.config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import react from '@vitejs/plugin-react'
 import ViteReactPlugin from '@vitejs/plugin-react';
 // import ReactRefreshPlugin from "@vitejs/plugin-react-refresh";
 // https://vitejs.dev/config/
